279d1c0a8ef0b54ada1752400d542d1cab7c2103
Leporoni <leporoni@hotmail.com>
2022-04-05 15:52:41 -0300

    first commit

---
+++ src/com/leporonitech/CsvPessoa.java
@@ -0,0 +1,35 @@
+package com.leporonitech;
+
+public class CsvPessoa {
+    private String nome;
+    private Integer idade;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return "CsvPessoa{nome='" + nome + "\', idade=" + idade + ", email='" + email + "\'}";
+    }
+}
---
+++ src/com/leporonitech/ReadRowAsObjectOpenCsv.java
@@ -0,0 +1,26 @@
+package com.leporonitech;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ReadRowAsObjectOpenCsv {
+
+    public static void main(String[] args) throws IOException {
+
+        Reader reader = Files.newBufferedReader(Paths.get("pessoas.csv"));
+
+        CsvToBean<CsvPessoa> csvToBean = new CsvToBeanBuilder(reader)
+                .withType(CsvPessoa.class)
+                .build();
+
+        List<CsvPessoa> pessoas = csvToBean.parse();
+
+        for (CsvPessoa pessoa : pessoas)
+            System.out.println(pessoa);
+    }
+}
